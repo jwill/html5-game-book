2dc1759aed1445d580839425148585052d992773
James Williams <james.l.williams@gmail.com>
2011-08-02 00:30:16 +0000

    See last commit message.

--- chapter9code/simple-nodejs/samplenodeapp.js
+++ chapter9code/simple-nodejs/samplenodeapp.js
@@ -3,4 +3,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen( 8124, "127.0.0.1");
+/* }).listen( process.env.C9_PORT, "0.0.0.0");    Uncomment to run on Cloud9IDE */
 console.log('Server running at http://127.0.0.1:8124/');
