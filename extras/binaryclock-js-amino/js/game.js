cadb0018b8afa3bdf16dd9f2d060d2555552d5ad
James Williams <james.l.williams@gmail.com>
2011-08-29 21:56:38 +0000

    Clock lines animating with a callback that checks the time.

--- extras/binaryclock-js-amino/js/game.js
+++ extras/binaryclock-js-amino/js/game.js
@@ -6,25 +6,80 @@ function Clock() {
     var self = this;
     
     self.init = function () {
-        self.currentTime = new Date()
-        self.d = new ClockLine()
-        self.d.drawSquares();
+        self.currentTime = new Date();
+        self.group = new Group();
+        self.drawLines();
+        
+        
+        
         var can = document.getElementById("gameboard");
         var runner = new Runner().setCanvas(can);
-        runner.root =self.d.components;
+        runner.root = self.group;
+        runner.addCallback(self.setTime);
         runner.start();
+    };
+    
+    self.drawLines = function () {
+        self.lines = [];
+        for (var i=0; i<6; i++) {
+            var line = new ClockLine()
+            self.lines.push(line);
+        }
+        
+        self.lines[0].setMaxValue(4);
+        self.lines[1].setMaxValue(4);
+        
+        self.lines[2].setMaxValue(4);
+        self.lines[3].setMaxValue(8);
+        
+        self.lines[4].setMaxValue(4);
+        self.lines[5].setMaxValue(8);
+        
+        // Alignment
+        self.lines[0].components.setY(60);
+        self.lines[1].components.setY(60);
+        self.lines[2].components.setY(60); 
+        self.lines[4].components.setY(60);
+        
+        for (i=0; i<6; i++) {
+            var line = self.lines[i];
+            line.drawSquares();
+            line.setX(i*(50+10));
+            self.group.add(self.lines[i].components);
+        }
     }
     
     self.setTime = function () {
         var time = new Date();
         var hrs, mins, secs;
+        var h1,h2,m1,m2,s1
         hrs = time.getHours();
+        // handle 12 hr vs 24 hr
+        //
         mins = time.getMinutes();
         secs = time.getSeconds();
         
-        // change the lines
+        hrs = hrs.toString().lpad("0",2);
+        mins = mins.toString().lpad("0",2);
+        secs = secs.toString().lpad("0",2);
+
+        h1 = hrs[0]; h2 = hrs[1];
+        m1 = mins[0]; m2 = mins[1];
+        s1 = secs[0]; s2 = secs[1];
         
-    }
+        self.lines[0].setValue(h1);
+        self.lines[1].setValue(h2);
+        
+        self.lines[2].setValue(m1);
+        self.lines[3].setValue(m2);
+        
+        self.lines[4].setValue(s1);
+        self.lines[5].setValue(s2);
+
+        // change the lines
+       // console.log(new Date());
+    };
+    
     self.init();
 }
 
@@ -36,14 +91,14 @@ function ClockLine() {
     var self = this;
     
     self.init = function() {
-        self.maxValue = 9; // default
+        self.maxValue = 8; // default
         self.sideLength = 50;
         self.components = new Group();
     };
     
     self.createNewRect = function() {
         return new Rect().set(0, 0, self.sideLength, self.sideLength);
-    }
+    };
     
     self.drawSquares = function () {
         var rect = [];
@@ -53,7 +108,7 @@ function ClockLine() {
         for(var j=0; j<4; j++) {
             rect[j].setY(j*(self.sideLength+10));
         }
-        if (self.maxValue == 9) {
+        if (self.maxValue == 8) {
             self.components.add(rect[0]).add(rect[1]).add(rect[2]).add(rect[3]);
         } else if (self.maxValue == 4) {
             self.components.add(rect[0]).add(rect[1]).add(rect[2]);
@@ -68,21 +123,36 @@ function ClockLine() {
     };
     
     self.setValue = function(val) {
-        var binaryText = val.toString(2);
-        if (self.maxValue == 9) {
+        var temp = Number(val);     // convert string to number
+        self.value = temp;
+        var binaryText = temp.toString(2);
+        if (self.maxValue == 8) {
             binaryText = binaryText.lpad("0", 4);
         } else if (self.maxValue == 4) {
             binaryText = binaryText.lpad("0", 3);
         } else {
             binaryText = binaryText.lpad("0", 2);
         }
+        var count = 0;
         for (var i=0; i<binaryText.length; i++) {
             var v = binaryText[i];
             if (v == "0") {
                 self.components.getChild(i).setFill("red");
-            } else self.components.getChild(i).setFill("green");
+            } else {
+                self.components.getChild(i).setFill("green");
+                count++;
+            }
         }
-    }
+        if (count == 4) {
+            console.log(new Date());
+        }
+    };
+    
+    self.setX = function (x) {
+        for (var i=0; i<self.components.childCount(); i++) {
+            self.components.getChild(i).setX(x);
+        }
+    };
     
     self.init();
 }
