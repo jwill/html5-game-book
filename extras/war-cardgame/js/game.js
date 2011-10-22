77fe8b25faef7ff25b659310b231b7cff09b9065
James Williams <james.l.williams@gmail.com>
2011-10-22 10:32:55 -0700

    War logic. TODO remove deps on underscore.js.

--- extras/war-cardgame/js/game.js
+++ extras/war-cardgame/js/game.js
@@ -20,6 +20,7 @@ function WarGame() {
     
 	self.init = function () {
 		self.group = new Group();
+        self.pile = [];
 		
 		var can = document.getElementById("gameboard");
 		self.runner = new Runner().setCanvas(can);
@@ -71,42 +72,96 @@ function WarGame() {
         self.runner.listen("MOUSE_PRESS", self.playerOneDeck, self.clickListener);
         self.runner.listen("COMPUTER_TURN", null, self.computerListener);
         self.runner.listen("EVALUATE", null, self.evaluate);
+        //self.runner.listen("WAR", null, self.setWar);
         
         self.group.add(self.playerCardText);
         self.group.add(self.computerCardText);
         self.group.add(self.playerOneDeck);
         self.group.add(self.computerDeck);
 	}
+	
+		self.setWar = function (evt) {
+			self.isWar = evt.isWar;
+		}
+		
+		self.layCards = function(cards, isComputer) {
+			var posDelta = isComputer ? 558 : 0
+			for (var i = 0; i < cards.length; i++) {
+				if (i == cards.length - 1) {
+					// Show face of card
+					cards[i].cardFront.x = 239 + posDelta;
+					cards[i].cardFront.y = 50 * (i + 1);
+					self.cardGroup.add(cards[i].cardFront);
+				} else {
+					cards[i].cardBack.x= 239 + posDelta;
+					cards[i].cardBack.y=50 * (i + 1);
+					self.cardGroup.add(cards[i].cardBack);
+				}
+			}
+		}
     
     self.clickListener = function (evt) {
+    		self.cardGroup.clear();
         // TODO Only if it is the players turn
-        var card = self.playerOne.playCard();
-        console.log(card);
-        if (card[0]) {
-            card[0].cardFront.x=239;
-            card[0].cardFront.y=50;
-            self.cardGroup.add(card[0].cardFront);
-            self.numPlayerCards--;
-            self.playerCardText.setText(self.numPlayerCards + " cards");
-            self.runner.fireEvent("COMPUTER_TURN", null, new ComputerEvent(1, [card[0]]))
+        if (!self.isWar) {
+            var card = self.playerOne.playCard();
+        	if (card[0]) {
+          	  card[0].cardFront.x=239;
+          	  card[0].cardFront.y=50;
+          	  self.cardGroup.add(card[0].cardFront);
+          	  self.numPlayerCards--;
+          	  self.playerCardText.setText(self.numPlayerCards + " cards");
+          	  self.runner.fireEvent("COMPUTER_TURN", null, new ComputerEvent(1, [card[0]]))
+        	}
+        } else {
+        	var numCards = self.playerOne.cards.length;
+        	var warCards = [];
+        	if (numCards >= 4) {
+        		_(4).times( function () {
+        			warCards.push(self.playerOne.playCard()[0]);
+        		});
+        	} else {
+        		_(numCards).times( function () {
+        			warCards.push(self.playerOne.playCard()[0]);
+        		});
+        	}
+        	// Draw card backs and one flipped over cards
+        	self.layCards(warCards, false);
+        	self.runner.fireEvent("COMPUTER_TURN", null, new ComputerEvent(2, warCards));
         }
         
     }
     
     self.computerListener = function (evt) {
         // TODO: Make event type for computer player
-        console.log(evt)
+        console.log(evt);
         if (evt.action === 1) { // Draw a single card
            var card = self.computer.playCard();
-        console.log(card);
-        if (card[0]) {
-            card[0].cardFront.x=797;
-            card[0].cardFront.y=50;
-            self.cardGroup.add(card[0].cardFront);
-            self.numComputerCards--;
-            self.computerCardText.setText(self.numComputerCards + " cards");
-            self.runner.fireEvent("EVALUATE", null, new EvaluateEvent(evt.playerCards,[card[0]]))
-        } 
+        	console.log(card);
+        	if (card[0]) {
+          	  card[0].cardFront.x=797;
+          	  card[0].cardFront.y=50;
+          	  self.cardGroup.add(card[0].cardFront);
+          	  self.numComputerCards--;
+          	  self.computerCardText.setText(self.numComputerCards + " cards");
+          	  self.runner.fireEvent("EVALUATE", null, new EvaluateEvent(evt.playerCards,[card[0]]))
+        	} 
+        } else if (evt.action === 2) { // War
+            var numCards = self.computer.cards.length;
+            var warCards = [];
+            if (numCards >= 4) {
+                _(4).times( function () {
+                    warCards.push(self.computer.playCard()[0]);
+                });
+            } else {
+                _(numCards).times( function () {
+                    warCards.push(self.computer.playCard()[0]);
+                });
+            }
+            // Draw card backs and one flipped over cards
+          self.layCards(warCards, true);
+          self.runner.fireEvent("EVALUATE", null, new EvaluateEvent(evt.playerCards, warCards));
+
         }
     }
     
@@ -116,26 +171,32 @@ function WarGame() {
     }
 	
 	self.evaluate = function(evaluateEvent) {
+        window.evaluateEvent = evaluateEvent;
 		var handOneSize = evaluateEvent.handOne.length;
         var handTwoSize = evaluateEvent.handTwo.length;
         
         var handOneCard = evaluateEvent.handOne[handOneSize-1];
         var handTwoCard = evaluateEvent.handTwo[handTwoSize-1];
-        var tempHand = null;
         
         console.log(evaluateEvent);
         
-        if (handOneCard.val > handTwoCard.val) {
+        if (handOneCard.val > handTwoCard.val || handTwoCard === undefined) {
             console.log("Player 1 wins!")
-            tempHand = evaluateEvent.handOne.concat(evaluateEvent.handTwo)
-            self.playerOne.addAll(tempHand)
-        } else if (handOneCard.val < handTwoCard.val) {
+            self.pile = self.pile.concat(evaluateEvent.handOne.concat(evaluateEvent.handTwo));
+            self.playerOne.addAll(self.pile)
+            self.pile = [];
+            self.isWar = false;
+        } else if (handOneCard.val < handTwoCard.val || handOneCard === undefined) {
             console.log("Computer wins!")
-            tempHand = evaluateEvent.handOne.concat(evaluateEvent.handTwo)
-            self.computer.addAll(tempHand)
+            self.pile = self.pile.concat(evaluateEvent.handOne.concat(evaluateEvent.handTwo));
+            self.computer.addAll(self.pile)
+            self.pile = [];
+            self.isWar = false;
         } else {
             // War   
             console.log("war");
+            self.isWar = true;
+            self.pile = self.pile.concat(evaluateEvent.handOne.concat(evaluateEvent.handTwo));
         }
         self.updateText();
 	}
